a4ed6a27d80e90a97ec5cbb573f2cf410167e8ed
akosilar <55524614+akosilar@users.noreply.github.com>
2023-10-29 02:22:01 -0400

    non click events

--- first-react-app/src/Clicker.jsx
+++ first-react-app/src/Clicker.jsx
@@ -2,10 +2,14 @@ function handleClick() {
     console.log('clicked the button')
 }
 
+function handleHover() {
+    console.log('hovered over')
+}
+
 export default function Clicker() {
     return (
         <div>
-            <p>click the button</p>
+            <p onMouseOver={handleHover}>click the button</p>
             <button onClick={handleClick}>Click</button>
         </div>
     )
